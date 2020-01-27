a5d692a16e2ea1c03b14769a8e565cf92f39de8e
Jelbuldin Ruslan <a01407036@unet.univie.ac.at>
2020-01-27 20:42:17 +0100

    hash function added

--- Slave/src/main/java/cc/task4/demo/Controller/NodeController.java
+++ Slave/src/main/java/cc/task4/demo/Controller/NodeController.java
@@ -1,5 +1,6 @@
 package cc.task4.demo.Controller;
 
+import cc.task4.demo.Exceptions.BucketDirectoryNotFound;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,14 +9,32 @@ import java.util.Map;
 public class NodeController {
 
     private Logger log;
+    private static final int BUCKET_SIZE = 6;
+
 
     public NodeController() {
         this.log = LoggerFactory.getLogger(NodeController.class);
     }
 
 
+    public String getFileName(Map<String, String> file) {
+        Map.Entry<String,String> entry = file.entrySet().iterator().next();
+        return entry.getKey();
+    }
+
+    public String getImageAsString(Map<String, String> file) {
+        Map.Entry<String,String> entry = file.entrySet().iterator().next();
+        return entry.getValue();
+    }
+
+
+
     private void saveFile(Map<String, String> file) {
 
+    }
+
+    private Integer getHashIndex(String imageInBytes) {
+        return imageInBytes.length() % BUCKET_SIZE;
     }
 
 
