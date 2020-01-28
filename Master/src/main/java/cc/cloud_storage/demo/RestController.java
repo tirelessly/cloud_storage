b2b7b7539334f56892649b4646a9df6a1005ec9b
Aigerim <aigerim@3kraft.com>
2020-01-28 19:37:49 +0100

    add delete endpoint

--- Master/src/main/java/cc/cloud_storage/demo/RestController.java
+++ Master/src/main/java/cc/cloud_storage/demo/RestController.java
@@ -7,9 +7,7 @@ import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.*;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,6 +15,7 @@ import java.io.File;
 import java.io.IOException;
 import java.sql.SQLOutput;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @org.springframework.web.bind.annotation.RestController
@@ -25,11 +24,11 @@ public class RestController {
     private Logger log = LoggerFactory.getLogger(RestController.class);
 
     @RequestMapping(value = "/api/v1/insert", method = RequestMethod.POST)
-    public @ResponseBody ResponseEntity<String> insert(String key, MultipartFile file) throws IOException {
+    public @ResponseBody ResponseEntity<String> insert(MultipartFile file) {
 
         try {
-            log.info("Received data to insert: {}", file.getName());
-            String response = masterNode.sendInsertRequestToNode(key, file);
+            log.info("Received data to insert: {}", file.getResource().getFilename());
+            String response = masterNode.sendInsertRequestToNode(file);
             return new ResponseEntity<>(response, HttpStatus.OK);
         } catch (Exception e) {
             e.printStackTrace();
@@ -38,9 +37,17 @@ public class RestController {
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
+    @RequestMapping(value = "/api/v1/delete/{key}", method = RequestMethod.DELETE)
+    public @ResponseBody ResponseEntity<String> delete(@PathVariable String key) {
+        log.info("Received key to delete: {}", key);
+        String response = masterNode.sendDeleteRequestToNode(key);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
 
     @RequestMapping(value = "/api/v1/status", method = RequestMethod.GET)
-    public @ResponseBody ResponseEntity<String> getStatus() throws IOException {
+    public @ResponseBody ResponseEntity<String> getStatus() {
         return new ResponseEntity<>("Master Node is up!", HttpStatus.OK);
     }
 }
--- Master/src/main/java/cc/cloud_storage/demo/master/MasterNode.java
+++ Master/src/main/java/cc/cloud_storage/demo/master/MasterNode.java
@@ -4,16 +4,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
+import sun.jvm.hotspot.runtime.Bytes;
+import sun.misc.BASE64Encoder;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.UnsupportedEncodingException;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MasterNode {
 
     private Map<Integer, String> endpoints = new HashMap<>();
     private static final String ENCODING = "ISO_8859_1";
-    private static final String IP = "78.104.29.82";
+    private static final String IP = "78.104.29.118";
 
     private Integer serviceNumber;
     private Logger log;
@@ -24,31 +32,42 @@ public class MasterNode {
         this.serviceNumber = this.endpoints.size();
     }
 
-    public String sendInsertRequestToNode(String key, MultipartFile file) {
-        String responseFromBucket = null;
+    public String sendInsertRequestToNode(MultipartFile file) {
+        String responseFromNode = null;
         try {
-            String bytesArr = new String(file.getName().getBytes(), ENCODING);
+            String bytesArr = new String(file.getResource().getFilename().getBytes(), ENCODING);
             String bucketUrl = getBucketURLByHashIndex(bytesArr);
 
             Map<String, String> dataToSend = new HashMap<>();
-            dataToSend.put(file.getName(),  new String(key.getBytes(), ENCODING));
+            dataToSend.put(file.getResource().getFilename(),  new String(file.getBytes(), ENCODING));
 
             log.info("About to send image to node: {}", bucketUrl);
-            responseFromBucket = new RestTemplate().postForObject(bucketUrl + "api/v1/insert", dataToSend, String.class);
-            log.info("Received response {} from node: {}", responseFromBucket, bucketUrl);
+            responseFromNode = new RestTemplate().postForObject(bucketUrl + "api/v1/insert", dataToSend, String.class);
+            log.info("Received response for insert request {} from node: {}", responseFromNode, bucketUrl);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return responseFromBucket;
+        return responseFromNode;
+    }
+
+    public String sendDeleteRequestToNode(String key){
+        String nodeUrl = getBucketURLByHashIndex(new String(key.getBytes()));
+        log.info("About to delete a value for key: {}", key);
+        String responseFromNode = new RestTemplate().getForObject(nodeUrl + "api/v1/delete/" + key, String.class);
+        log.info("Received response for delete request {} from node {}", responseFromNode, nodeUrl);
+
+        return responseFromNode;
     }
 
     private String getBucketURLByHashIndex(String fileName) {
         Integer hashIndex = fileName.length() % serviceNumber;
+        log.info("Hash index is {}", hashIndex);
         return this.endpoints.get(hashIndex);
     }
 
     private void addEndpoints() {
+        this.endpoints.put(0, "http://" + IP + ":8080/");
         this.endpoints.put(1, "http://" + IP + ":8080/");
         this.endpoints.put(2, "http://" + IP + ":8080/");
         this.endpoints.put(3, "http://" + IP + ":8080/");
